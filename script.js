3a48779d73f28cb7b0c5c465b057e8fcaff2b369
Gaspar Molochnik <Gaspar.fm@gmail.com>
2022-10-14 15:49:02 -0600

    Mi primer calculeitor

---
+++ script.js
@@ -0,0 +1,44 @@
+function mensaje(num){
+  switch (num) {
+    case 0:
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 1: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 2: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 3: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 4: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 5: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 6: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 7: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 8: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break; 
+    case 9: 
+      console.log("Se ha presionado el valor " + num);
+      alert("Se ha presionado el valor " + num);
+      break;
+  }
+}
