ccdad0cdaf5447b5ad6ae0fd33087bc190328217
Gaspar Molochnik <Gaspar.fm@gmail.com>
2022-10-16 19:44:31 -0600

    Co-authored-by: Dalí <dalesius@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function mensaje(num){
-  console.log("Se ha presionado el valor " + num);
-  alert("Se ha presionado el valor " + num);
+function mensaje(num) {
+  let numDisplay = document.getElementById("display");
+  numDisplay.innerHTML = num;
 }
+
+// TODO: leer el link que te pase y aprender a agregar un div en
+// un 'contenedor' debajo de la 'calculadora'
+
+// TODO: crear funcion que tome el elemento que ha sido clickeado
+// function tomarElementoClickeado(...) {
+// Tomar elemento clickeado
+// hacer que el numero se ponga color blanco
+// }
+
+// TODO: crear funcion que contenga las dos creadas para ser
+// ejecutada cuando hay un click
+
+// TODO: crear una funcion que agregue un div a ese contenedor
+// en la parte superior del contenedor con el numero
+// presionado. (Los divs anteriores deben desplazarse
+// hacia abajao en forma de columna)
