d6a61470af1356f353754645517e9b1056eb658c
Gaspar Molochnik <Gaspar.fm@gmail.com>
2022-10-19 17:26:48 -0600

    Agregué botón para agregar div al final en JS

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function mensaje(num) {
-  let numDisplay = document.getElementById("display");
-  numDisplay.innerHTML = num;
+    let numDisplay = document.getElementById("display");
+    numDisplay.innerHTML = num;
 }
-
-// TODO: leer el link que te pase y aprender a agregar un div en
-// un 'contenedor' debajo de la 'calculadora'
+function addDiv() {
+    let elemento = document.getElementById("contenedor");
+    let agregarDiv = document.createElement("div");
+    elemento.insertBefore(agregarDiv, null);
+}
 
 // TODO: crear funcion que tome el elemento que ha sido clickeado
 // function tomarElementoClickeado(...) {
@@ -19,3 +21,8 @@ function mensaje(num) {
 // en la parte superior del contenedor con el numero
 // presionado. (Los divs anteriores deben desplazarse
 // hacia abajao en forma de columna)
+
+//    HECHO
+
+// TODO: leer el link que te pase y aprender a agregar un div en
+// un 'contenedor' debajo de la 'calculadora'
