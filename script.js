5cad033889c4e47ea9701d197a5567bbb0d9d619
Gaspar Molochnik <Gaspar.fm@gmail.com>
2022-10-19 18:25:27 -0600

    Creada la funcion de tomarElementoClicleado

--- script.js
+++ script.js
@@ -7,12 +7,40 @@ function addDiv() {
     let agregarDiv = document.createElement("div");
     elemento.insertBefore(agregarDiv, null);
 }
-
-// TODO: crear funcion que tome el elemento que ha sido clickeado
-// function tomarElementoClickeado(...) {
-// Tomar elemento clickeado
-// hacer que el numero se ponga color blanco
-// }
+function tomarElementoClickeado(num) {
+    switch (num) {
+        case 0:
+            document.getElementById("boton0").style.color = "white";
+            break;
+        case 1:
+            document.getElementById("boton1").style.color = "white";
+            break;
+        case 2:
+            document.getElementById("boton2").style.color = "white";
+            break;
+        case 3:
+            document.getElementById("boton3").style.color = "white";
+            break;
+        case 4:
+            document.getElementById("boton4").style.color = "white";
+            break;
+        case 5:
+            document.getElementById("boton5").style.color = "white";
+            break;
+        case 6:
+            document.getElementById("boton6").style.color = "white";
+            break;
+        case 7:
+            document.getElementById("boton7").style.color = "white";
+            break;
+        case 8:
+            document.getElementById("boton8").style.color = "white";
+            break;
+        case 9:
+            document.getElementById("boton9").style.color = "white";
+            break;
+    }
+}
 
 // TODO: crear funcion que contenga las dos creadas para ser
 // ejecutada cuando hay un click
@@ -26,3 +54,9 @@ function addDiv() {
 
 // TODO: leer el link que te pase y aprender a agregar un div en
 // un 'contenedor' debajo de la 'calculadora'
+
+// TODO: crear funcion que tome el elemento que ha sido clickeado
+// function tomarElementoClickeado(...) {
+// Tomar elemento clickeado
+// hacer que el numero se ponga color blanco
+// }
